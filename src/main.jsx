8aed2da8b684b4a8fb705ff30c4db3f757964e78
kalyanimurkute <murkutekalyani259@gmail.com>
2026-03-04 13:22:16 +0530

    initial commit

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { BrowserRouter , Routes, Route} from "react-router";
+
+const root = createRoot(document.getElementById('root'));
+ root.render(<BrowserRouter>
+ <Routes>
+  <Route path='/' element={<h1>Home</h1>}/>
+
+ </Routes>
+ </BrowserRouter>
+ );
