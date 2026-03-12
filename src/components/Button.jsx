05030ae4b79099e7e5646409b616e207565268eb
kalyanimurkute <murkutekalyani259@gmail.com>
2026-03-12 22:26:06 +0530

    add button sections

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,13 +1,13 @@
 const BUTTON_VARIANTS = {
-    primary: 'rounded-full bg-[#F39F9F] text-blacK hover:bg-[#FFC29B] cursor-pointer',
-    secondary: 'rounded-full bg-[#B95E82] text-white  hover:bg-[#F39F9F] cursor-pointer',
-    tertiary: 'rounded-full bg-transparent border-2 border-[#B95E82] text-[#B95E82]  hover:bg-[#B95E82] hover:text-white cursor-pointer'
+    primary: 'rounded-full bg-[#F39F9F] text-blacK hover:bg-[#FFC29B] transition-duration-200 cursor-pointer',
+    secondary: 'rounded-full bg-[#B95E82] text-white  hover:bg-[#F39F9F] transition-duration-300 cursor-pointer',
+    tertiary: 'rounded-full bg-transparent border-2 border-[#B95E82] text-[#B95E82] font-bold hover:bg-[#B95E82] hover:text-white transition-duration-400 cursor-pointer'
 }
 
 const BUTTON_SIZES = {
-    small: 'text-sm py-1 px-3',
-    medium: 'text-md py-2 px-6',
-    large: 'text-lg py-3 px-8'
+    small: 'text-sm py-1 px-3 m-3',
+    medium: 'text-md py-2 px-6 m-2',
+    large: 'text-lg py-3 px-8 m-1'
 }
 
 function Button({title, onclick, variant = 'primary', size = 'medium'}){
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -11,14 +11,17 @@ function Home() {
       <Navbar />
 
       <div className='min-h-screen '>
-       <div className='flex flex-col md:flex-row items-center justify-center gap-15 mt-10'>
-        <img src={Image} alt="Vegetable Background" className="h-[300px]" />
-        <div>
-        <h6 className='text-[40px]! font-bold text-center md:text-[50px]! text-[#B95E82]' >one the platform for all <br />the vegetable you need.</h6>
-        <Button title={"call us"} />
-      </div>
-      </div>
-      <FeatureSection />
+        <div className='flex flex-col md:flex-row items-center justify-center gap-20 mt-10'>
+          <img src={Image} alt="Vegetable Background" className="h-[300px]" />
+          <div>
+            <h6 className='text-[40px]! font-bold text-center md:text-[50px]! text-[#B95E82]' >one the platform for all<br /> the vegetable you need.</h6>
+           <div className='flex flex-col md:flex-row gap-4 mt-8 justify-center'>
+            <Button title={"Explore Now"} variant='primary' size='medium' />
+            <Button title={"call us"} variant='tertiary' size='medium' />
+          </div>
+          </div>
+        </div>
+        <FeatureSection />
       </div>
       <Footer />
     </div>
